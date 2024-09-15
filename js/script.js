e722add4d6b9b16ee832483e92ccf5ed9433395c
houdareda <106600533+houdareda@users.noreply.github.com>
2024-09-15 03:43:21 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -36,3 +36,81 @@ let path = patharr.join("/");
 let host = window.location.protocol + "//" + window.location.host + path;
 let redirectValue = document.getElementById("redDir").value;
 document.getElementById("redDir").value = `${host}/thanksPage.html`;
+
+
+
+
+const Mobile_Number = document.getElementById("password");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+
+const submit_Form =  () => {
+
+    
+    let messages = [];
+
+  var Mobile_Number_Value = Mobile_Number.value;
+  var first_Three_NUM = Mobile_Number_Value.substring(0, 3);
+
+  if ( Mobile_Number.value.length < 11 || (first_Three_NUM !== "010" && first_Three_NUM !== "011" && first_Three_NUM !== "012" && first_Three_NUM !== "015" && first_Three_NUM !== "٠١٠" && first_Three_NUM !== "٠١١" && first_Three_NUM !== "٠١٢" && first_Three_NUM !== "٠١٥")) {
+    messages.push("رقم الهاتف غير صحيح !");
+  }
+
+  if (Mobile_Number.value.length > 11) {
+    messages.push("رقم الهاتف غير صحيح !");
+  }
+
+  if (messages.length > 0) {
+    errorElement.innerText = messages.join(", ");
+    errorElement.classList.add("active")
+
+  }else{
+    CheckNumber_Repeat(Mobile_Number.value)
+   
+  }
+
+
+};
+
+
+const CheckNumber_Repeat = (user_Number) =>{
+
+    const data = {
+        Number: user_Number,
+    };
+
+    fetch('https://leads-data.store/api/user/store', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          phone: user_Number
+        }),
+        
+    })
+
+    .then(response => {
+        return response.json();
+      })
+
+    .then(data => {
+          var Number_Check = data.success
+          console.log(Number_Check);
+
+
+          if(Number_Check === true){
+            form.submit()
+          }else{
+            errorElement.innerText = data.message;
+            errorElement.classList.add("active")
+          }
+           
+      })
+      .catch((error) => {
+        form.submit()
+      })
+
+}
+
